6f03a4d0f7b728d0873a04643b47ca6413e6d9fd
ismaelrr888 <ismaelrr888@gmail.com>
2021-08-06 04:41:29 -0400

    add sort by year + title

--- src/modules/movie/components/MoviesList.js
+++ src/modules/movie/components/MoviesList.js
@@ -2,15 +2,32 @@ import {
   CircularProgress,
   Grid,
   TablePagination,
+  TableSortLabel,
   Typography,
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeMovieFilters, getMovies } from "../../../actions/movie";
 import MovieFilters from "../components/MovieFilters";
 import MovieItem from "../components/MovieItem";
 
+const useStyles = makeStyles(() => ({
+  visuallyHidden: {
+    border: 0,
+    clip: "rect(0 0 0 0)",
+    height: 1,
+    margin: -1,
+    overflow: "hidden",
+    padding: 0,
+    position: "absolute",
+    top: 20,
+    width: 1,
+  },
+}));
+
 export default function MoviesList() {
+  const classes = useStyles();
   const { moviesData, filters, loadingMovies } = useSelector(
     (state) => state.movie
   );
@@ -18,6 +35,24 @@ export default function MoviesList() {
 
   const handlePageChange = (event, newPage) => {
     dispatch(changeMovieFilters({ name: "page", value: newPage + 1 }));
+  };
+
+  const createSortHandlerYear = () => {
+    dispatch(
+      changeMovieFilters({
+        name: "order",
+        value: filters.order === "releaseYear" ? "-releaseYear" : "releaseYear",
+      })
+    );
+  };
+
+  const createSortHandlerTitle = () => {
+    dispatch(
+      changeMovieFilters({
+        name: "order",
+        value: filters.order === "title" ? "-title" : "title",
+      })
+    );
   };
 
   useEffect(() => {
@@ -41,7 +76,38 @@ export default function MoviesList() {
               alignItems={"center"}
               direction={"row"}
             >
-              <Grid item></Grid>
+              <Grid item>
+                {moviesData?.results?.length > 0 && (
+                  <>
+                    <TableSortLabel
+                      active={true}
+                      direction={
+                        filters.order === "releaseYear" ? "asc" : "desc"
+                      }
+                      onClick={createSortHandlerYear}
+                    >
+                      Year
+                      <span className={classes.visuallyHidden}>
+                        {filters.order === "releaseYear"
+                          ? "sorted descending"
+                          : "sorted ascending"}
+                      </span>
+                    </TableSortLabel>
+                    <TableSortLabel
+                      active={true}
+                      direction={filters.order === "title" ? "asc" : "desc"}
+                      onClick={createSortHandlerTitle}
+                    >
+                      Title
+                      <span className={classes.visuallyHidden}>
+                        {filters.order === "title"
+                          ? "sorted descending"
+                          : "sorted ascending"}
+                      </span>
+                    </TableSortLabel>
+                  </>
+                )}
+              </Grid>
               <Grid item>
                 <TablePagination
                   labelRowsPerPage={""}
@@ -54,6 +120,7 @@ export default function MoviesList() {
                 />
               </Grid>
             </Grid>
+
             {moviesData?.results?.map((item, index) => (
               <Grid item key={index}>
                 <MovieItem movie={item} />
