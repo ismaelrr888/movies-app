bc384b214d1721e35c01cde1f2325b967a0230c9
ismaelrr888 <ismaelrr888@gmail.com>
2021-08-06 03:11:34 -0400

    add paginate to movies list

--- src/modules/movie/components/MoviesList.js
+++ src/modules/movie/components/MoviesList.js
@@ -1,12 +1,17 @@
-import { Grid } from "@material-ui/core";
+import { Grid, TablePagination } from "@material-ui/core";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getMovies } from "../../../actions/movie";
+import { changeMovieFilters, getMovies } from "../../../actions/movie";
 import MovieItem from "../components/MovieItem";
 
 export default function MoviesList() {
   const { moviesData, filters } = useSelector((state) => state.movie);
   const dispatch = useDispatch();
+
+  const handlePageChange = (event, newPage) => {
+    console.log(newPage);
+    dispatch(changeMovieFilters({ name: "page", value: newPage + 1 }));
+  };
 
   useEffect(() => {
     dispatch(getMovies());
@@ -15,6 +20,26 @@ export default function MoviesList() {
   return (
     <>
       <Grid container spacing={4}>
+        <Grid
+          item
+          container
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          direction={"row"}
+        >
+          <Grid item></Grid>
+          <Grid item>
+            <TablePagination
+              labelRowsPerPage={""}
+              component={"div"}
+              rowsPerPageOptions={[10]}
+              page={moviesData?.total === 0 ? 0 : filters.page - 1}
+              count={moviesData?.total ? moviesData?.total : 0}
+              rowsPerPage={10}
+              onPageChange={handlePageChange}
+            />
+          </Grid>
+        </Grid>
         {moviesData?.results?.map((item, index) => (
           <Grid item key={index}>
             <MovieItem movie={item} />
